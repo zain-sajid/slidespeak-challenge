d841afe25fa81c307002e04e0d071ddfce2e82c2
Zain Sajid <zsajid.bese19seecs@seecs.edu.pk>
2025-05-29 22:41:38 +0200

    feat: remove frontend_url env from docker-compose

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     aws_secret_access_key: str
     aws_region: str
     aws_s3_bucket: str
-    frontend_url: str
+    frontend_url: str = "http://localhost:3000"
     redis_url: str
     unoserver_url: str
     model_config = SettingsConfigDict(env_file=".env")
